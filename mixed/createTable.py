f2f55400fbd01532d4ee18b0f4867dbf1c088e96
Aliweea <wzn40899@163.com>
2018-01-04 23:36:02 +0800

    change the mixed database

--- mixed/createTable.py
+++ mixed/createTable.py
@@ -16,8 +16,7 @@ DEFAULT CHARACTER SET = utf8;"""
 
 # create table t_movie
 create_movie = """CREATE TABLE `mixeddb`.`t_movie` (
-  `movie_id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `product_id` varchar(40) DEFAULT NULL,
+  `movie_id` varchar(40) DEFAULT NULL,
   `movie_name` varchar(400) NOT NULL,
   `average_rating` float DEFAULT NULL,
   `studio` varchar(100) DEFAULT NULL,
@@ -28,32 +27,12 @@ create_movie = """CREATE TABLE `mixeddb`.`t_movie` (
   `year` int(4) DEFAULT NULL,
   `month` int(2) DEFAULT NULL,
   `day` int(2) DEFAULT NULL,
-  `language_name` varchar(100) DEFAULT NULL,
-  `genre_name` varchar(45) DEFAULT NULL,
-  `director_id` int(11) NOT NULL DEFAULT '0',
-  `actor_id` int(11) NOT NULL DEFAULT '0',
-  `review_id` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`movie_id`,`director_id`,`actor_id`,`review_id`),
-  KEY `fk1_director_idx` (`director_id`),
-  KEY `fk2_actor_idx` (`actor_id`),
-  KEY `fk3_time_idx` (`year`,`day`,`month`),
-  KEY `fk4_review_idx` (`review_id`),
+  `languages` varchar(500) DEFAULT NULL,
+  `genres` varchar(500) DEFAULT NULL,
+  `directors` VARCHAR(1000) DEFAULT NULL,
+  `actors` VARCHAR(1000) DEFAULT NULL,
+  PRIMARY KEY (`movie_id`),
   KEY `fk_movie_to_time` (`year`,`month`,`day`),
-  CONSTRAINT `fk4_movie_to_review` 
-    FOREIGN KEY (`review_id`) 
-    REFERENCES `t_review` (`review_id`) 
-    ON DELETE CASCADE 
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk1_movie_to_director` 
-    FOREIGN KEY (`director_id`) 
-    REFERENCES `t_director` (`director_id`) 
-    ON DELETE CASCADE 
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk2_movie_to_actor` 
-    FOREIGN KEY (`actor_id`) 
-    REFERENCES `t_actor` (`actor_id`) 
-    ON DELETE CASCADE 
-    ON UPDATE CASCADE,
   CONSTRAINT `fk_movie_to_time` 
     FOREIGN KEY (`year`, `month`, `day`) 
     REFERENCES `t_time` (`year`, `month`, `day`) 
@@ -61,24 +40,6 @@ create_movie = """CREATE TABLE `mixeddb`.`t_movie` (
     ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 """
-
-# create table t_director
-create_director = """CREATE TABLE `mixeddb`.`t_director` (
-  `director_id` INT NOT NULL AUTO_INCREMENT,
-  `director_name` VARCHAR(150) NOT NULL,
-  PRIMARY KEY (`director_id`),
-  UNIQUE KEY `director_name_UNIQUE` (`director_name`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;"""
-
-# create table t_actor
-create_actor = """CREATE TABLE `mixeddb`.`t_actor` (
-  `actor_id` INT NOT NULL AUTO_INCREMENT,
-  `actor_name` VARCHAR(250) NOT NULL,
-  PRIMARY KEY (`actor_id`),
-  UNIQUE KEY `actor_name_UNIQUE` (`actor_name`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;"""
 
 # create table t_user
 create_user = """CREATE TABLE `mixeddb`.`t_user` (
@@ -116,15 +77,11 @@ def recreate_mixed_table():
     # 删除表的顺序与创建表相反
     cursor.execute("DROP TABLE IF EXISTS t_movie")
     cursor.execute("DROP TABLE IF EXISTS t_time")
-    cursor.execute("DROP TABLE IF EXISTS t_actor")
-    cursor.execute("DROP TABLE IF EXISTS t_director")
     # cursor.execute("DROP TABLE IF EXISTS t_review")
     # cursor.execute("DROP TABLE IF EXISTS t_user")
     # 创建表
     # cursor.execute(create_user)
     # cursor.execute(create_review)
-    cursor.execute(create_director)
-    cursor.execute(create_actor)
     cursor.execute(create_time)
     cursor.execute(create_movie)
 
--- mixed/processMovie.py
+++ mixed/processMovie.py
@@ -9,45 +9,15 @@ from mixed.createTable import *
 
 # ------------------- SQL BEGIN ---------------------------
 
-# SQL 插入t_director语句
-sql_director = "INSERT IGNORE INTO t_director (director_name) VALUES (%s)"
-
-# SQL 插入t_actor语句
-sql_actor = "INSERT IGNORE INTO t_actor (actor_name) VALUES (%s)"
-
 # SQL 插入t_time语句
 sql_time = "INSERT IGNORE INTO t_time (year, month, day, season, weekday) VALUES (%s, %s, %s, %s, %s)"
 
 # SQL 插入t_movie语句
 sql_movie = """INSERT IGNORE INTO t_movie
-(product_id, movie_name, average_rating, studio, mpaa_rating, runtime, description, rank, year, month, day,
-language_name, genre_name, director_id, actor_id, review_id)
-SELECT * FROM
-(SELECT %s as col_1, %s as col_2, %s as col_3, %s as col_4, %s as col_5, %s as col_6, %s as col_7,
-%s as col_8, %s as col_9, %s as col_10, %s as col_11, %s as col_12, %s as col_13,
-td.director_id as col_14, ta.actor_id as col_15, %s as col_16
-FROM t_director td, t_actor ta
-WHERE td.director_name=%s and ta.actor_name=%s) AS tb;
+(movie_id, movie_name, average_rating, studio, mpaa_rating, runtime, description, rank, year, month, day,
+languages, genres, directors, actors)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
 """
-
-# # SQL 插入t_movie语句
-# sql_movie = """INSERT IGNORE INTO t_movie
-# (movie_id, movie_name, average_rating, studio, mpaa_rating, runtime, description, rank, year, month, day,
-# genre_name, language_name, director_id, actor_id, review_id)
-# VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-# """
-#
-# # SQL 通过director_name查找director_id
-# query_director = "SELECT director_id FROM t_director WHERE director_name=%s"
-#
-# # SQL 通过actor_name查找actor_id
-# query_actor = "SELECT actor_id FROM t_actor WHERE actor_name=%s"
-
-# SQL 通过movie_id查找review_id
-query_review = """SELECT review_id FROM t_review WHERE movie_id=%s"""
-
-# SQL 删除t_review的movie_id字段
-delete_review_col = "ALTER TABLE `mixeddb`.`t_review` DROP COLUMN `movie_id`;"
 
 # --------------------- SQL END ------------------------
 
@@ -55,104 +25,13 @@ delete_review_col = "ALTER TABLE `mixeddb`.`t_review` DROP COLUMN `movie_id`;"
 # -----------------  VARIABLE BEGIN --------------------
 
 # 储存有sql语句的param元组的list
-param_director = []
-param_actor = []
 param_time = []
 param_movie = []
 
-movie_id = ''
-movie_name = ''
-languages = ''
-directors = ''
-starring = ''
-supporting_actors = ''
-release_date = ''
-average_rating = ''
-studio = ''
-mpaa_rating = ''
-genres = ''
-runtime = ''
-description = ''
-rank = ''
-
 # ------------------ VARIABLE END ----------------------
-
-
-def process_line_data():
-    global release_date, param_time
-    if release_date is not None:
-        date_time = time.strptime(release_date, '%B %d, %Y')
-        season = getSeason(date_time.tm_mon)
-        param_time.append((date_time.tm_year, date_time.tm_mon, date_time.tm_mday, season, date_time.tm_wday))
-        process_language(date_time)
-    else:
-        process_language(None)
-
-
-def process_language(date_time):
-    global languages
-    if languages is not None:
-        languages = languages.split(", ")
-        for language_name in languages:
-            process_genre(date_time, language_name)
-    else:
-        process_genre(date_time, None)
-
-
-def process_genre(date_time, language_name):
-    global genres
-    if genres is not None:
-        for genre_name in genres:
-            process_director(date_time, language_name, genre_name)
-    else:
-        process_director(date_time, language_name, None)
-
-
-def process_director(date_time, language_name, genre_name):
-    global directors, param_director
-    if directors is not None:
-        for director_name in directors:
-            param_director.append((director_name,))
-            process_actor(date_time, language_name, genre_name, director_name)
-    else:
-        process_actor(date_time, language_name, genre_name, None)
-
-
-def process_actor(date_time, language_name, genre_name, director_name):
-    global param_actor, starring, supporting_actors
-    if starring is not None:
-        for actor_name in starring:
-            param_actor.append((actor_name,))
-            process_review(date_time, language_name, genre_name, director_name, actor_name)
-    elif supporting_actors is not None:
-        for actor_name in supporting_actors:
-            param_actor.append((actor_name,))
-            process_review(date_time, language_name, genre_name, director_name, actor_name)
-    else:
-        process_review(date_time, language_name, genre_name, director_name, None)
-
-
-def process_review(date_time, language_name, genre_name, director_name, actor_name):
-    global param_actor, param_movie, starring, supporting_actors, movie_id, movie_name, \
-        average_rating, studio, mpaa_rating, runtime, description, rank
-    cursor.execute(query_review, (movie_id,))
-    results = cursor.fetchall()
-    if results is not None:
-        for row in results:
-            if date_time is not None:
-                param_movie.append((movie_id, movie_name, average_rating, studio, mpaa_rating, runtime, description,
-                                    rank, date_time.tm_year, date_time.tm_mon, date_time.tm_mday,
-                                    language_name, genre_name, row[0], director_name, actor_name))
-            else:
-                param_movie.append((movie_id, movie_name, average_rating, studio, mpaa_rating, runtime, description,
-                                    rank, None, None, None,
-                                    language_name, genre_name, row[0], director_name, actor_name))
-
 
 def execute_insert_many():
     global param_director, param_actor, param_time, param_movie
-    cursor.executemany(sql_director, param_director)
-    cursor.executemany(sql_actor, param_actor)
     cursor.executemany(sql_time, param_time)
     cursor.executemany(sql_movie, param_movie)
     db.commit()
@@ -160,14 +39,12 @@ def execute_insert_many():
 
 def clear_params():
     global param_director, param_actor, param_time, param_movie
-    param_director = []
-    param_actor = []
     param_time = []
     param_movie = []
 
 
 def load_data(filename):
-    global param_director, param_actor, param_time, param_movie,\
+    global param_director, param_movie,\
         movie_id, movie_name, languages, directors, starring, supporting_actors, \
         release_date, average_rating, studio, mpaa_rating, genres, runtime, description, rank
     with codecs.open(filename, encoding='utf-8') as f:
@@ -191,9 +68,53 @@ def load_data(filename):
             runtime = data.get("runtime")
             description = data.get("desc")
             rank = data.get("rank")
-            process_line_data()
 
-            if nrow % 100 == 0:
+            str_languag = ''
+            if languages is not None:
+                languages = languages.split(", ")
+                for language_name in languages:
+                    str_languag += ',' + language_name
+                languages = str_languag
+
+            if genres  is not None:
+                str_genre = ''
+                for genre_name in genres:
+                    str_genre += ',' + genre_name
+                genres = str_genre
+
+            if directors is not None:
+                str_director = ''
+                for director_name in directors:
+                    str_director += ',' + director_name
+                directors = str_director
+
+            str_actor = ''
+            actors = None
+            if starring is not None:
+                for actor_name in starring:
+                    str_actor += ',' + actor_name
+
+            if supporting_actors is not None:
+                for actor_name in supporting_actors:
+                    str_actor += ',' + actor_name
+
+            if str_actor != '':
+                actors = str_actor
+
+            if release_date is not None:
+                date_time = time.strptime(release_date, '%B %d, %Y')
+                season = getSeason(date_time.tm_mon)
+                param_time.append((date_time.tm_year, date_time.tm_mon, date_time.tm_mday,
+                                   season, date_time.tm_wday))
+                param_movie.append((movie_id, movie_name, average_rating, studio, mpaa_rating, runtime, description,
+                                    rank, date_time.tm_year, date_time.tm_mon, date_time.tm_mday,
+                                    languages, genres, directors, actors))
+            else:
+                param_movie.append((movie_id, movie_name, average_rating, studio, mpaa_rating, runtime, description,
+                                    rank, None, None, None,
+                                    languages, genres, directors, actors))
+
+            if nrow % 5000 == 0:
                 execute_insert_many()
                 clear_params()
                 print nrow, ' done!'
@@ -207,7 +128,7 @@ def load_data(filename):
 
 if __name__=="__main__":
 
-    # recreate_mixed_table()
+    recreate_mixed_table()
 
     # 打开数据库连接
     db = MySQLdb.connect("localhost", "root", "123456", "mixeddb")
@@ -222,8 +143,6 @@ if __name__=="__main__":
     load_data('../data/amazon3.json')
     load_data('../data/amazon4.json')
     load_data('../data/amazon5.json')
-
-    # cursor.execute(delete_review_col)
 
     db.commit()
 
